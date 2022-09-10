015af401fd2b8f1cce9aedc810a82293a9576230
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-09-10 11:18:23 -0300

    Adding summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header/indext";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 }
