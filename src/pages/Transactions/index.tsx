8142d974a170cc2889b207ae97116aac2fae08d7
Thiagohrcosta <thiagohrcosta86@gmail.com>
2022-12-23 05:32:49 -0300

    formatting values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/indext";
 import { Summary } from "../../components/Summary";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -38,19 +39,20 @@ export function Transactions() {
         <TransactionsTable>
           <tbody>
             {transactions.map(transaction => {
-              return (
-                <tr key={transaction.id}>
-                  <td width="50%">{transaction.description}</td>
-                  <td>
-                    <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
-                    </PriceHighlight>
-                  </td>
-                  <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
-                </tr>
-              )
-            })}
+                return (
+                  <tr key={transaction.id}>
+                    <td width="50%">{transaction.description}</td>
+                    <td>
+                      <PriceHighlight variant={transaction.type}>
+                        {transaction.type === 'outcome' && '- '}
+                        {priceFormatter.format(transaction.price)}
+                      </PriceHighlight>
+                    </td>
+                    <td>{transaction.category}</td>
+                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                  </tr>
+                )
+              })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
